6921d0a3484dc841ebcd0f7a7ac5dc52783930b8
fevos4 <fevatesfish@gmail.com>
2024-09-16 14:20:05 +0300

    Updated again

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+// Import components
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
@@ -7,22 +9,32 @@ import Skills from "./Components/Skills";
 import Portfolio from "./Components/Portfolio";
 import Contact from "./Components/Contact";
 
+// Optional Layout component
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+    <About />
+    <Skills />
+    <Portfolio />
+    <Contact />
+  </>
+);
 
-export default function App() {
-   return (
-      
-               <>
-                  <Navbar/>
-                  <Hero/>
-                  <About/>
-                  <Skills/>
-                  <Portfolio/>
-                  <Contact/>
-               
-                 
-               </>
-      
-          
-     
-   );
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
+
+export default App;
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -6,18 +6,30 @@ import "slick-carousel/slick/slick-theme.css";
 
 const Portfolio = () => {
   const [filter, setFilter] = useState("all");
+  const [showMore, setShowMore] = useState(false);
 
   const projects = [
-    { image: "/assets/port.jpeg", category: "websites" },
-    { image: "/assets/port2.jpeg", category: "designs" },
-    { image: "/assets/port3.jpeg", category: "websites" },
-    { image: "/assets/port4.jpeg", category: "designs" },
-    { image: "/assets/port5.jpeg", category: "websites" },
-    { image: "/assets/port6.jpeg", category: "designs" },
+    { image: "/assets/coffee.jpeg", category: "websites", link: "https://coffee-website-murex-pi.vercel.app/" },
+    { image: "/assets/health.png", category: "designs", link: "https://www.figma.com/proto/8dpQoMKdZdpupjFwD6uFVW/Health_Knock_Design_0?page-id=0%3A1&node-id=25-17&node-type=frame&viewport=362%2C512%2C0.16&t=AyfFGuBA1tbgnNMo-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=25%3A17" },
+    { image: "/assets/michu.jpeg", category: "websites", link: "https://michu-website.vercel.app/" },
+    { image: "/assets/michudd.png", category: "designs", link: "https://www.figma.com/proto/0OhLeJC9uhytAimhX6y5lz/Michu-Tech?page-id=0%3A1&node-id=92-4&viewport=-100%2C1046%2C0.21&t=9nOtvb5mRNI7Tmiv-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=92%3A4" },
+    { image: "/assets/michu2.jpeg", category: "websites", link: "https://michu-tech-web.vercel.app/" },
+    { image: "/assets/bankk.png", category: "designs", link: "https://www.figma.com/proto/4I61Sj8CM8KLTPafXSpfQS/Untitled?page-id=0%3A1&node-id=1-2&viewport=310%2C217%2C0.09&t=mDkYIwY4kNy6FNO0-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=1%3A2" },
+    { image: "/assets/bank.jpeg", category: "websites", link: "https://bank-infos.vercel.app/" },
+    { image: "/assets/portt.jpeg", category: "websites", link: "https://my-portfolio-beta-nine-99.vercel.app/" },
+    { image: "/assets/coffeed.png", category: "designs", link: "https://www.figma.com/proto/bpSKQ0AwWl49G7xm4BEWJj/Untitled?page-id=0%3A1&node-id=1-3&node-type=frame&viewport=645%2C379%2C0.19&t=BCCavcSmosQNkclk-1&scaling=scale-down&content-scaling=fixed" },
+    { image: "/assets/defi.jpeg", category: "websites", link: "https://de-fi-project.vercel.app/" },
+    { image: "/assets/winey.png", category: "designs", link: "/projects/project4" },
+    
+    { image: "/assets/wineyb.png", category: "designs", link: "/projects/project6" },
   ];
 
   const filteredProjects =
     filter === "all" ? projects : projects.filter((project) => project.category === filter);
+
+  // Calculate the initial and additional projects to show
+  const initialProjects = filteredProjects.slice(0, 6); // Show 6 projects initially
+  const additionalProjects = filteredProjects.slice(6);
 
   // Carousel settings for smaller screens
   const settings = {
@@ -29,7 +41,7 @@ const Portfolio = () => {
   };
 
   return (
-    <div className="md:min-h-screen bg-[#090C11] text-white p-10 md:p-24">
+    <div className="md:min-h-screen bg-[#090C11] text-white p-10 md:p-24" id="portfolio">
       <div className="mb-10">
         <h2 className="text-sm font-bolder text-yellow-500">My</h2>
         <h1 className="text-lg md:text-3xl text-white font-bolder mt-2 mb-4">Portfolio</h1>
@@ -47,16 +59,17 @@ const Portfolio = () => {
           </button>
         ))}
       </div>
-      <div className="hidden md:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 text-center">
+      <div className="hidden md:grid grid-cols-1 md:grid-cols-3 gap-10 text-center">
         <AnimatePresence>
-          {filteredProjects.map((project, index) => (
+          {(showMore ? filteredProjects : initialProjects).map((project, index) => (
             <motion.div
               key={index}
-              className="bg-gray-800 rounded-lg overflow-hidden shadow-lg"
+              className="bg-gray-800 rounded-lg overflow-hidden shadow-lg cursor-pointer"
               initial={{ opacity: 0, y: 30 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: 30 }} // Smooth exit animation
               transition={{ duration: 0.4, ease: "easeInOut" }} // Duration for smooth transitions
+              onClick={() => window.open(project.link, '_blank')}
             >
               <img
                 src={project.image}
@@ -67,16 +80,25 @@ const Portfolio = () => {
           ))}
         </AnimatePresence>
       </div>
+      <div className="flex justify-center mt-4">
+        <button
+          onClick={() => setShowMore(!showMore)}
+          className="bg-yellow-500 text-black py-2 px-4 rounded-lg font-bolder"
+        >
+          {showMore ? 'Show Less' : 'Show More'}
+        </button>
+      </div>
       <div className="md:hidden">
         <Slider {...settings}>
           {filteredProjects.map((project, index) => (
             <motion.div
               key={index}
-              className="bg-gray-800 rounded-lg overflow-hidden shadow-lg"
+              className="bg-gray-800 rounded-lg overflow-hidden shadow-lg cursor-pointer"
               initial={{ opacity: 0, y: 30 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: 30 }} // Smooth exit animation
               transition={{ duration: 0.4, ease: "easeInOut" }} // Duration for smooth transitions
+              onClick={() => window.open(project.link, '_blank')}
             >
               <img
                 src={project.image}
